be6c2ef62864b10f52e4ccae188bb74e495141f1
Mikaele <mikaelethainara@gmail.com>
2022-08-22 15:39:29 -0300

    create sucess and error page

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # login-dti
-O repositório é referente a um teste de front-end no qual é criado um tela de login utilizando uma api de autenticação da Microsoft 
+O repositório é referente a um teste de front-end no qual é criado um tela de login
+
+Para teste ultilize:
+Usuário: teste@teste.com
+Senha:123
--- script.js
+++ script.js
@@ -3,9 +3,21 @@ function Enviar() {
   var senha = document.getElementById("senha");
 
 if ((email.value == "teste@teste.com") && (senha.value == "123")) {
-  alert('Funcionou!');
+  SucessoLogin();
 }
 else {
-  alert('Ops!');
+  ErroLogin();
 }
+}
+
+function SucessoLogin(){
+  window.location="sucesso-login.html";
+}
+
+function ErroLogin(){
+  window.location="erro-login.html";
+}
+
+function Logout() {
+  window.location="index.html";
 }
